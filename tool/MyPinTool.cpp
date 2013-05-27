2b2002b223784c7ae21a868bf1462a796c492917
Sam Kerr <kerr.sam@gmail.com>
2013-05-26 20:32:27 -0700

    Now logs all instruction addresses and the images they are run from.
    Addition of Boost library and makefile rules necessary to accomodate it.
    
    Edit to gitignore.

--- tool/MyPinTool.cpp
+++ tool/MyPinTool.cpp
@@ -7,112 +7,57 @@
 #include "pin.H"
 #include <iostream>
 #include <fstream>
-
-/* ================================================================== */
-// Global variables 
-/* ================================================================== */
-
-UINT64 insCount = 0;        //number of dynamically executed instructions
-UINT64 bblCount = 0;        //number of dynamically executed basic blocks
-UINT64 threadCount = 0;     //total number of threads, including main thread
+#include <boost/icl/split_interval_map.hpp>
+#include <string>
 
 std::ostream * out = &cerr;
 
-/* ===================================================================== */
-// Command line switches
-/* ===================================================================== */
-KNOB<string> KnobOutputFile(KNOB_MODE_WRITEONCE,  "pintool",
-    "o", "", "specify file name for MyPinTool output");
+boost::icl::interval_map<int, std::string> address_lib_interval_map;
 
-KNOB<BOOL>   KnobCount(KNOB_MODE_WRITEONCE,  "pintool",
-    "count", "1", "count instructions, basic blocks and threads in the application");
-
-
-/* ===================================================================== */
-// Utilities
-/* ===================================================================== */
-
-/*!
- *  Print out help message.
- */
-INT32 Usage()
+void instructionTrace(INS ins, void* v)
 {
-    cerr << "This tool prints out the number of dynamically executed " << endl <<
-            "instructions, basic blocks and threads in the application." << endl << endl;
+    printf("Instruction Address: 0x%08x : ", (int)INS_Address(ins));
 
-    cerr << KNOB_BASE::StringKnobSummary() << endl;
+    boost::icl::interval_map<int, std::string>::iterator it = address_lib_interval_map.begin();
 
-    return -1;
-}
-
-/* ===================================================================== */
-// Analysis routines
-/* ===================================================================== */
-
-/*!
- * Increase counter of the executed basic blocks and instructions.
- * This function is called for every basic block when it is about to be executed.
- * @param[in]   numInstInBbl    number of instructions in the basic block
- * @note use atomic operations for multi-threaded applications
- */
-VOID CountBbl(UINT32 numInstInBbl)
-{
-    bblCount++;
-    insCount += numInstInBbl;
-}
-
-/* ===================================================================== */
-// Instrumentation callbacks
-/* ===================================================================== */
-
-/*!
- * Insert call to the CountBbl() analysis routine before every basic block 
- * of the trace.
- * This function is called every time a new trace is encountered.
- * @param[in]   trace    trace to be instrumented
- * @param[in]   v        value specified by the tool in the TRACE_AddInstrumentFunction
- *                       function call
- */
-VOID Trace(TRACE trace, VOID *v)
-{
-    // Visit every basic block in the trace
-    for (BBL bbl = TRACE_BblHead(trace); BBL_Valid(bbl); bbl = BBL_Next(bbl))
+    while(it != address_lib_interval_map.end())
     {
-        // Insert a call to CountBbl() before every basic bloc, passing the number of instructions
-        BBL_InsertCall(bbl, IPOINT_BEFORE, (AFUNPTR)CountBbl, IARG_UINT32, BBL_NumIns(bbl), IARG_END);
+        boost::icl::discrete_interval<int> range = (*it).first;
+        std::string name = (*it++).second;
+        
+        if(boost::icl::contains(range, (int)INS_Address(ins)))
+        {
+            printf("%s", name.c_str());
+            //std::cout << range << ":" << name << std::endl;
+            break;
+        }
     }
-}
+    printf("\n");
+ }
 
-/*!
- * Increase counter of threads in the application.
- * This function is called for every thread created by the application when it is
- * about to start running (including the root thread).
- * @param[in]   threadIndex     ID assigned by PIN to the new thread
- * @param[in]   ctxt            initial register state for the new thread
- * @param[in]   flags           thread creation flags (OS specific)
- * @param[in]   v               value specified by the tool in the 
- *                              PIN_AddThreadStartFunction function call
- */
-VOID ThreadStart(THREADID threadIndex, CONTEXT *ctxt, INT32 flags, VOID *v)
+void imageLoaded(IMG img, void* data)
 {
-    threadCount++;
-}
+    //printf("Image loaded\n");
+    printf("------\n");
+    printf("Loading Image: %s\n", IMG_Name(img).c_str());
+    printf("\tLoading Location:\n");
+    printf("\t0x%08x to 0x%08x\n", (int)IMG_LowAddress(img), (int)IMG_HighAddress(img));
+    printf("\tFirst Instruction address: 0x%08x\n", (int)IMG_Entry(img));
+    //printf("\tFirst Section: 0x%08x\n", (int)IMG_SecHead(img));
+    //printf("\tLast Section: 0x%08x\n", (int)IMG_SecTail(img));
+    printf("------\n");
+    boost::icl::discrete_interval<int> itv = boost::icl::discrete_interval<int>::right_open(
+        (int)IMG_LowAddress(img),
+        (int)IMG_HighAddress(img)
+    );
 
-/*!
- * Print out analysis results.
- * This function is called when the application exits.
- * @param[in]   code            exit code of the application
- * @param[in]   v               value specified by the tool in the 
- *                              PIN_AddFiniFunction function call
- */
-VOID Fini(INT32 code, VOID *v)
-{
-    *out <<  "===============================================" << endl;
-    *out <<  "MyPinTool analysis results: " << endl;
-    *out <<  "Number of instructions: " << insCount  << endl;
-    *out <<  "Number of basic blocks: " << bblCount  << endl;
-    *out <<  "Number of threads: " << threadCount  << endl;
-    *out <<  "===============================================" << endl;
+    address_lib_interval_map.add(
+        make_pair(
+            itv,
+            std::string(IMG_Name(img))
+        )
+        );
+
 }
 
 /*!
@@ -126,34 +71,10 @@ int main(int argc, char *argv[])
 {
     // Initialize PIN library. Print help message if -h(elp) is specified
     // in the command line or the command line is invalid 
-    if( PIN_Init(argc,argv) )
-    {
-        return Usage();
-    }
+    PIN_Init(argc,argv);
     
-    string fileName = KnobOutputFile.Value();
-
-    if (!fileName.empty()) { out = new std::ofstream(fileName.c_str());}
-
-    if (KnobCount)
-    {
-        // Register function to be called to instrument traces
-        TRACE_AddInstrumentFunction(Trace, 0);
-
-        // Register function to be called for every thread before it starts running
-        PIN_AddThreadStartFunction(ThreadStart, 0);
-
-        // Register function to be called when the application exits
-        PIN_AddFiniFunction(Fini, 0);
-    }
-    
-    cerr <<  "===============================================" << endl;
-    cerr <<  "This application is instrumented by MyPinTool" << endl;
-    if (!KnobOutputFile.Value().empty()) 
-    {
-        cerr << "See file " << KnobOutputFile.Value() << " for analysis results" << endl;
-    }
-    cerr <<  "===============================================" << endl;
+    IMG_AddInstrumentFunction(imageLoaded, 0);
+    INS_AddInstrumentFunction(instructionTrace, 0);
 
     // Start the program, never returns
     PIN_StartProgram();
--- tool/build.sh
+++ tool/build.sh
@@ -1,1 +1,2 @@
+#make APP_INCLUDES="-I /usr/local/boost/include" PIN_ROOT=../pin-2.12-58423-gcc.4.4.7-linux/ $1
 make PIN_ROOT=../pin-2.12-58423-gcc.4.4.7-linux/ $1
